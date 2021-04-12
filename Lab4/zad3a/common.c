00b317283637d42a3b01e30db1ed8e1980819d29
Piotr Płoszczyca <piotrus.plo@gmail.com>
2021-04-12 17:39:03 +0200

    Put some common code from two files to one

---
+++ Lab4/zad3a/common.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int countSignals = 0;
+int HANDLE_SIGNALS = 1;
+char *signalSenderType;
+
+void endSignalHandler(int sig_no, siginfo_t *sig_info, void *ucontext);
+
+void sendWithKill(int catcherPID, int nOfSignals, int startSignal, int endSignal){
+    for(int i = 0; i < nOfSignals; i++){
+        kill(catcherPID, startSignal);
+    }
+    kill(catcherPID, endSignal);
+}
+
+void sendWithSigqueue(int catcherPID, int nOfSignals){
+    union sigval value;
+    value.sival_int = countSignals;
+
+    for(int i = 0; i < nOfSignals; i++){
+        sigqueue(catcherPID, SIGUSR1, value);
+    }
+
+    sigqueue(catcherPID, SIGUSR2, value);
+}
+
+void sendRealSignalWithKill(int catcherPID, int nOfSignals){
+    sendWithKill(catcherPID, nOfSignals, SIGRTMIN+10, SIGRTMIN+15);
+}
+
+void handleSignal(int sig_no){
+    countSignals++;
+}
+
+void setStartEndSignal(int *startSignal, int *endSignal){
+    if(strcmp(signalSenderType, "SIGRT") == 0){
+        *startSignal = SIGRTMIN+10;
+        *endSignal = SIGRTMIN+15;
+    } else {    
+
+        *endSignal = SIGUSR2;
+    }
+}
+
+void chooseSignalMethod(int catcherPID, int nOfSignals){
+    if(strcmp(signalSenderType, "KILL") == 0){
+        sendWithKill(catcherPID, nOfSignals, SIGUSR1, SIGUSR2);
+    } else if(strcmp(signalSenderType, "SIGQUEUE") == 0){
+        sendWithSigqueue(catcherPID, nOfSignals);
+    } else if(strcmp(signalSenderType, "SIGRT") == 0){
+        sendRealSignalWithKill(catcherPID, nOfSignals);
+    }
+}
+
+void setMasksAndHandlers(int startSignal, int endSignal, sigset_t *mask){
+    struct sigaction endAction;
+
+    endAction.sa_sigaction = endSignalHandler;
+    sigemptyset(&endAction.sa_mask);
+    endAction.sa_flags = SA_SIGINFO;
+    sigaction(endSignal, &endAction, NULL);
+
+    sigfillset(mask);
+    sigdelset(mask, startSignal);
+    sigdelset(mask, endSignal);
+}
--- Lab4/zad3a/sender.c
+++ Lab4/zad3a/sender.c
@@ -1,37 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <stdlib.h>
-
-int countSignals = 0;
-int HANDLE_SIGNALS = 1;
-char *signalSenderType;
-
-void sendWithKill(int catcherPID, int nOfSignals, int startSignal, int endSignal){
-    for(int i = 0; i < nOfSignals; i++){
-        kill(catcherPID, startSignal);
-    }
-    kill(catcherPID, endSignal);
-}
-
-void sendWithSigqueue(int catcherPID, int nOfSignals){
-    union sigval value;
-
-    for(int i = 0; i < nOfSignals; i++){
-        sigqueue(catcherPID, SIGUSR1, value);
-    }
-
-    sigqueue(catcherPID, SIGUSR2, value);
-}
-
-void sendRealSignalWithKill(int catcherPID, int nOfSignals){
-    sendWithKill(catcherPID, nOfSignals, SIGRTMIN+10, SIGRTMIN+15);
-}
-
-void handleSignal(int sig_no){
-    countSignals++;
-}
+#include "common.c"
 
 void endSignalHandler(int sig_no, siginfo_t *sig_info, void *ucontext){
     HANDLE_SIGNALS = 0;
@@ -43,34 +10,9 @@ void endSignalHandler(int sig_no, siginfo_t *sig_info, void *ucontext){
 }
 
 int main(int argc, char *argv[]){
-    int catcherPID;
-    int nOfSignals;
+    int startSignal, endSignal, nOfSignals, catcherPID;
     sigset_t mask;
-    struct sigaction endAction;
-    signalSenderType = argv[3];
-
-    int startSignal, endSignal;
-
-    if(strcmp(signalSenderType, "SIGRT") == 0){
-        startSignal = SIGRTMIN+10;
-        endSignal = SIGRTMIN+15;
-    } else {
-        startSignal = SIGUSR1;
-        endSignal = SIGUSR2;
-    }
-
-    signal(startSignal, handleSignal);
-
-    endAction.sa_sigaction = endSignalHandler;
-    sigemptyset(&endAction.sa_mask);
-    endAction.sa_flags = SA_SIGINFO;
-    sigaction(endSignal, &endAction, NULL);
-
-
-    sigfillset(&mask);
-    sigdelset(&mask, startSignal);
-    sigdelset(&mask, endSignal);
-
+    
     if(argc != 4){
         printf("Bad ammount of arguments\n");
         exit(1);
@@ -78,14 +20,15 @@ int main(int argc, char *argv[]){
 
     catcherPID = atoi(argv[1]);
     nOfSignals = atoi(argv[2]);
+    signalSenderType = argv[3];
 
-    if(strcmp(argv[3], "KILL") == 0){
-        sendWithKill(catcherPID, nOfSignals, SIGUSR1, SIGUSR2);
-    } else if(strcmp(argv[3], "SIGQUEUE") == 0){
-        sendWithSigqueue(catcherPID, nOfSignals);
-    } else if(strcmp(argv[3], "SIGRT") == 0){
-        sendRealSignalWithKill(catcherPID, nOfSignals);
-    }
+    setStartEndSignal(&startSignal, &endSignal);
+
+    signal(startSignal, handleSignal);
+
+    setMasksAndHandlers(startSignal, endSignal, &mask);
+    
+    chooseSignalMethod(catcherPID, nOfSignals);
 
     while(HANDLE_SIGNALS == 1){
         sigsuspend(&mask);
